0a77dfcf6bb09ab56510614fda947daa1aaacb36
Nikhil Khedekar <nikhilvkhedekar@gmail.com>
2018-03-22 23:06:36 +0530

    with PID

--- src/scripts/offb_reciever.py
+++ src/scripts/offb_reciever.py
@@ -9,43 +9,56 @@ from mavros_msgs.msg import State
 
 current_state = State()
 pose = PoseStamped()
+vel = TwistStamped()
+vel.twist.linear.x = 0
+vel.twist.linear.y = 00
+vel.twist.linear.z = 00
 
 rospy.init_node('offb_node', anonymous=True)
 rate = rospy.Rate(100)
+
+last_error = numpy.zeros((3))
+e_sum = 0
+
+kp = 2
+kd = 0.3
+ki = 0.000
 
 def state_cb(msg):
 	global current_state
 	current_state = msg
 	#print "callback state"
 
-#def waypoint_reciever():
-#	rospy.Subscriber("wayponits", PoseStamped, cb_move)
-
-"""
-def cb_move(msg):
-	#print "subscribed"
-	#pose = msg
-	local_pos_pub.publish(msg)
-	print msg
-	rate.sleep()
-"""
-
 def cb_move(msg):
 	global pose
 	point = msg
 	#local_pos_pub.publish(point)
-	local_vel_pub.publish(vel)
+	#local_vel_pub.publish(vel)
 	pose = point
 	#rate.sleep()
 	#print pose
 
 def pose_cb(msg):
+	global e_sum
 	global pose
+	global last_error
 	#print "in pose_cb"
 	desired_pose = numpy.array((pose.pose.position.x, pose.pose.position.y, pose.pose.position.z))
 	current_pose = numpy.array((msg.pose.position.x, msg.pose.position.y, msg.pose.position.z))
-	print numpy.linalg.norm(desired_pose - current_pose)
-	print desired_pose[0] - current_pose[0], desired_pose[1] - current_pose[1] 
+	#print numpy.linalg.norm(numpy.array((desired_pose[0] - current_pose[0]),(desired_pose[1] - current_pose[1])))
+	print(numpy.linalg.norm(desired_pose - current_pose))
+	#print desired_pose[0] - current_pose[0], desired_pose[1] - current_pose[1], desired_pose[2] - current_pose[2]
+
+	error = desired_pose - current_pose
+
+	e_sum += error
+
+	vel.twist.linear.x = kp*error[0] + kd*(error[0] - last_error[0]) + ki*e_sum[0]
+	vel.twist.linear.y = kp*error[1] + kd*(error[1] - last_error[1]) + ki*e_sum[1]
+	vel.twist.linear.z = kp*error[2] + kd*(error[2] - last_error[2]) + ki*e_sum[2]
+
+	local_vel_pub.publish(vel)
+
 
 
 if __name__=="__main__":
@@ -54,7 +67,6 @@ if __name__=="__main__":
 		
 	rospy.Subscriber("mavros/state", State, state_cb)
 	local_vel_pub = rospy.Publisher('mavros/setpoint_velocity/cmd_vel', TwistStamped, queue_size=10)
-	
 
 	local_pos_pub = rospy.Publisher('mavros/setpoint_position/local', PoseStamped, queue_size=10)
 	print("Publisher and Subscriber Created")
@@ -63,22 +75,14 @@ if __name__=="__main__":
 	set_mode_client = rospy.ServiceProxy('mavros/set_mode', SetMode)
 	print("Clients Created")
 
-	
 	while(not current_state.connected):
 		#print(current_state.connected)
 		rate.sleep()
 	
-	
 	#set position here
 	pose.pose.position.x = 0
 	pose.pose.position.y = 0
-	pose.pose.position.z = 2
-	
-	vel = TwistStamped()
-	vel.twist.linear.x = 200
-	vel.twist.linear.y = 200
-	vel.twist.linear.z = 200
-		
+	pose.pose.position.z = 2	
 
 	for i in range(100):
 		local_pos_pub.publish(pose)
--- src/scripts/waypoint_pub.py
+++ src/scripts/waypoint_pub.py
@@ -13,20 +13,32 @@ import tf2_ros
 
 way_pub = rospy.Publisher('waypoints', PoseStamped, queue_size=1)
 rospy.init_node('waypoint_pub', anonymous=True)
-rate = rospy.Rate(100)
+rate = rospy.Rate(10)
 
 def talker():
 	waypoint = PoseStamped()
-	waypoint.pose.position.x = -10
-	waypoint.pose.position.y = -10
+	waypoint.pose.position.x = -100
+	waypoint.pose.position.y = -100
 	waypoint.pose.position.z = 7
 	t = 0
 	
-	#time.sleep(10)
+	#time.sleep(100)
 	while not rospy.is_shutdown():
-		for i in range(-1000, 1000):
+		waypoint.pose.position.x = 10*math.cos(t)
+		waypoint.pose.position.y = 10*math.sin(t)
+		q1 = tf.transformations.quaternion_from_euler(0, 0, 0)
+		waypoint.pose.orientation.x = q1[0]
+		waypoint.pose.orientation.y = q1[1]
+		waypoint.pose.orientation.z = q1[2]
+		waypoint.pose.orientation.w = q1[3]
+		way_pub.publish(waypoint)
+		t+= 0.05
+		print waypoint
+		rate.sleep()
+	"""
+		for i in range(-10000, 10000):
 			print "cycle 1"
-			waypoint.pose.position.y = -10
+			waypoint.pose.position.y = -100
 			waypoint.pose.position.x = (float(i))/100
 			q1 = tf.transformations.quaternion_from_euler(0, 0, 0)
 			waypoint.pose.orientation.x = q1[0]
@@ -38,9 +50,9 @@ def talker():
 			print waypoint
 			rate.sleep()
 		
-		for i in range(-1000, 1000):
+		for i in range(-10000, 10000):
 			print "cycle 2"
-			waypoint.pose.position.x = 10
+			waypoint.pose.position.x = 100
 			waypoint.pose.position.y = (float(i))/100
 			q1 = tf.transformations.quaternion_from_euler(0, 0, 0)
 			waypoint.pose.orientation.x = q1[0]
@@ -52,9 +64,9 @@ def talker():
 			print waypoint
 			rate.sleep()
 
-		for i in range(-1000,1000):
+		for i in range(-10000,10000):
 			print "cycle 3"
-			waypoint.pose.position.y = 10
+			waypoint.pose.position.y = 100
 			waypoint.pose.position.x = (float(-i))/100
 			q1 = tf.transformations.quaternion_from_euler(0, 0, 0)
 			waypoint.pose.orientation.x = q1[0]
@@ -66,9 +78,9 @@ def talker():
 			print waypoint
 			rate.sleep()
 
-		for i in range(-1000,1000):
+		for i in range(-10000,10000):
 			print "cycle 4"
-			waypoint.pose.position.x = -10
+			waypoint.pose.position.x = -100
 			waypoint.pose.position.y = (float(-i))/100
 			q1 = tf.transformations.quaternion_from_euler(0, 0, 0)
 			waypoint.pose.orientation.x = q1[0]
@@ -79,6 +91,7 @@ def talker():
 			t += 0.05
 			print waypoint
 			rate.sleep()
+			"""
 
 		
 		
